6cf8834078d986e95c8e082c66fec05f6e3a32ce
syntaxSizer <kakuma4never@gmail.com>
2016-06-17 16:28:19 +0300

    create an intent and pass the data to the intent

--- app/src/main/java/matrial/aka/twitter/MainActivity.java
+++ app/src/main/java/matrial/aka/twitter/MainActivity.java
@@ -1,5 +1,6 @@
 package matrial.aka.twitter;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -11,6 +12,7 @@ import com.twitter.sdk.android.core.TwitterAuthConfig;
 import com.twitter.sdk.android.core.TwitterException;
 import com.twitter.sdk.android.core.TwitterSession;
 import com.twitter.sdk.android.core.identity.TwitterLoginButton;
+import com.twitter.sdk.android.core.models.User;
 
 import io.fabric.sdk.android.Fabric;
 
@@ -20,13 +22,12 @@ public class MainActivity extends AppCompatActivity {
     private static final String TWITTER_KEY = "GqwOQXxIw66SOW6gltFgi8caU";
     private static final String TWITTER_SECRET = "mqr5G1ZwDHZt9nfGYZhKMbkwFlt6680hMuitsPISdsGPkhubKy";
 
-// tags that will hold the retrived username and profile image after successful login
-    public static final String KEY_USER="name";
-    public static final String profile_img_url="image_url";
+    // tags that will hold the retrived username and profile image after successful login
+    public static final String KEY_USER = "name";
+    public static final String profile_img_url = "image_url";
 
-// login button
- TwitterLoginButton twitterLoginButton;
-
+    // login button
+    TwitterLoginButton twitterLoginButton;
 
 
     @Override
@@ -42,19 +43,73 @@ public class MainActivity extends AppCompatActivity {
         twitterLoginButton = (TwitterLoginButton) findViewById(R.id.twitter_login_button);
 
         // add callback to the button
-        twitterLoginButton.setCallback(new Callback<TwitterSession>(){
+        twitterLoginButton.setCallback(new Callback<TwitterSession>() {
             @Override
             public void success(Result<TwitterSession> result) {
-            // if login success we pass the result objct to login method
-             login(result);
+                // if login success we pass the result objct to login method
+                login(result);
             }
 
             @Override
             public void failure(TwitterException exception) {
                 // in case the login failed this will print
-                Log.d("TwitterKit","Login with twitter failure", exception);
+                Log.d("TwitterKit", "Login with twitter failure", exception);
             }
 
+        });
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        //Adding the login result back to the button
+        twitterLoginButton.onActivityResult(requestCode, resultCode, data);
+    }
+
+
+
+
+    //method to accept the result object
+    public void login(Result<TwitterSession>result) {
+
+        TwitterSession session = result.data;
+
+        //getting the user from the session
+        final String username = session.getUserName();
+
+        //this code will fetch the profile image url
+        //getting the account service of the user logged in
+        Twitter.getApiClient(session).getAccountService().verifyCredentials(true, false, new Callback<User>() {
+            @Override
+            public void failure(TwitterException exception) {
+// here we handle error
             }
-        }
+
+            @Override
+            public void success(Result<User> result) {
+                // if succeed the create  a user object from user.Result.data
+                User user = userResult.data;
+
+                //getting the user profile image url
+                String profileimage = user.profileImageUrl.replace("_normal", "");
+
+                //creating an intent
+                Intent intent = new Intent(MainActivity.this, ProfileActivity.class);
+
+                // add ing the values to the intent
+                intent.putExtra(KEY_USER, username);
+                intent.putExtra(profile_img_url, profileimage);
+
+                //start activity
+                startActivity(intent);
+
+
+            }
+
+
+        });
+
+
+    }
 }
