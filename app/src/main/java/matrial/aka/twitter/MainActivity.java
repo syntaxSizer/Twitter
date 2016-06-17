b92b014bed47e61acce14f54cc92657fb5d829af
syntaxSizer <kakuma4never@gmail.com>
2016-06-17 15:58:12 +0300

    add callback to login button

--- app/src/main/java/matrial/aka/twitter/MainActivity.java
+++ app/src/main/java/matrial/aka/twitter/MainActivity.java
@@ -1,9 +1,15 @@
 package matrial.aka.twitter;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
 import com.twitter.sdk.android.Twitter;
+import com.twitter.sdk.android.core.Callback;
+import com.twitter.sdk.android.core.Result;
 import com.twitter.sdk.android.core.TwitterAuthConfig;
+import com.twitter.sdk.android.core.TwitterException;
+import com.twitter.sdk.android.core.TwitterSession;
 import com.twitter.sdk.android.core.identity.TwitterLoginButton;
 
 import io.fabric.sdk.android.Fabric;
@@ -26,8 +32,29 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //Initializing TwitterAuthConfig, these two line will also added automatically while configuration we did
         TwitterAuthConfig authConfig = new TwitterAuthConfig(TWITTER_KEY, TWITTER_SECRET);
         Fabric.with(this, new Twitter(authConfig));
         setContentView(R.layout.activity_main);
-    }
+
+        //initialize twitter login button
+        twitterLoginButton = (TwitterLoginButton) findViewById(R.id.twitter_login_button);
+
+        // add callback to the button
+        twitterLoginButton.setCallback(new Callback<TwitterSession>(){
+            @Override
+            public void success(Result<TwitterSession> result) {
+            // if login success we pass the result objct to login method
+             login(result);
+            }
+
+            @Override
+            public void failure(TwitterException exception) {
+                // in case the login failed this will print
+                Log.d("TwitterKit","Login with twitter failure", exception);
+            }
+
+            }
+        }
 }
