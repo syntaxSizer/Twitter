c1ca1a23f96ba7b48632db1c3981ded766cadc78
syntaxSizer <kakuma4never@gmail.com>
2016-06-17 15:42:04 +0300

    add profile activity

--- app/src/main/java/matrial/aka/twitter/MainActivity.java
+++ app/src/main/java/matrial/aka/twitter/MainActivity.java
@@ -2,12 +2,22 @@ package matrial.aka.twitter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.twitter.sdk.android.Twitter;
+import com.twitter.sdk.android.core.TwitterAuthConfig;
+import io.fabric.sdk.android.Fabric;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Note: Your consumer key and secret should be obfuscated in your source code before shipping.
+    private static final String TWITTER_KEY = "GqwOQXxIw66SOW6gltFgi8caU";
+    private static final String TWITTER_SECRET = "mqr5G1ZwDHZt9nfGYZhKMbkwFlt6680hMuitsPISdsGPkhubKy";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        TwitterAuthConfig authConfig = new TwitterAuthConfig(TWITTER_KEY, TWITTER_SECRET);
+        Fabric.with(this, new Twitter(authConfig));
         setContentView(R.layout.activity_main);
     }
 }
---
+++ app/src/main/java/matrial/aka/twitter/ProfileActivity.java
@@ -0,0 +1,7 @@
+package matrial.aka.twitter;
+
+/**
+ * Created by geckozila on 17/06/16.
+ */
+public class ProfileActivity {
+}
